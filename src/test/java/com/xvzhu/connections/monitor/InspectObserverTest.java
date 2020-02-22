e57e1c5abe6614428288e5ba4698be99cdaaf89b
Zhang Sai <195412063@qq.com>
2020-02-22 13:04:21 +0800

    Add unit test for inspect observer.

--- src/test/java/com/xvzhu/connections/monitor/InspectObserverTest.java
+++ src/test/java/com/xvzhu/connections/monitor/InspectObserverTest.java
@@ -1,6 +1,20 @@
 package com.xvzhu.connections.monitor;
 
+import com.xvzhu.connections.BasicSftpClientConnectionManager;
+import com.xvzhu.connections.apis.ConnectionBean;
+import com.xvzhu.connections.apis.ConnectionManagerConfig;
+import com.xvzhu.connections.apis.IConnectionManager;
+import com.xvzhu.connections.apis.IObserver;
+import com.xvzhu.connections.apis.ManagerBean;
+import com.xvzhu.connections.data.ConnectionBeanBuilder;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author : xvzhu
@@ -8,23 +22,39 @@ import org.junit.Test;
  * @since Date : 2020-02-16 12:44
  */
 public class InspectObserverTest {
-    @Test
-    public void should_schedule_close_timed_out_connection_when_basic_connection_timed_out() {
+    private ConnectionManagerConfig connectionManagerConfig = ConnectionManagerConfig.builder().build();
 
+    private IObserver observer = new InspectObserver();
+
+    @Test
+    public void should_release_connection_when_borrow_timed_out() {
+        IConnectionManager connectionManager = BasicSftpClientConnectionManager.builder().build();
+        ConnectionBean connectionBean = ConnectionBeanBuilder.builder().build().getConnectionBean();
+        Map<Thread, ManagerBean> managerMap = new HashMap<>();
+        long timeNow = Calendar.getInstance().getTimeInMillis();
+        ManagerBean managerBean = ManagerBean.builder().borrowTime(timeNow - connectionManagerConfig.getBorrowTimeoutMS() - 100000).build();
+        managerMap.put(Thread.currentThread(), managerBean);
+
+        Map<ConnectionBean, Map<Thread, ManagerBean>> connections = new HashMap<>();
+        connections.put(connectionBean, managerMap);
+
+        observer.visit(connectionManager, connectionBean, connections);
+        assertFalse(managerBean.isConnectionBorrowed());
     }
 
     @Test
     public void should_schedule_reset_connection_container_when_basic_connection_is_closed() {
+        IConnectionManager connectionManager = BasicSftpClientConnectionManager.builder().build();
+        ConnectionBean connectionBean = ConnectionBeanBuilder.builder().build().getConnectionBean();
+        Map<Thread, ManagerBean> managerMap = new HashMap<>();
+        long timeNow = Calendar.getInstance().getTimeInMillis();
+        ManagerBean managerBean = ManagerBean.builder().borrowTime(timeNow - connectionManagerConfig.getBorrowTimeoutMS() + 10000).build();
+        managerMap.put(Thread.currentThread(), managerBean);
 
-    }
+        Map<ConnectionBean, Map<Thread, ManagerBean>> connections = new HashMap<>();
+        connections.put(connectionBean, managerMap);
 
-    @Test
-    public void should_schedule_close_timed_out_connection_when_pooled_connection_timed_out() {
-
-    }
-
-    @Test
-    public void should_schedule_reset_connection_container_when_pooled_connection_is_closed() {
-
+        observer.visit(connectionManager, connectionBean, connections);
+        assertTrue(managerBean.isConnectionBorrowed());
     }
 }
